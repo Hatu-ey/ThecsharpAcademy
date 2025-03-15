f244ac86062034e1e3e7e046801c77ba14d553ad
4ECHatu <tuan.ha@4ec.eu>
2025-03-15 10:38:05 +0100

    Math Game Base
    Simple Math Game project to begin with:
    https://thecsharpacademy.com/project/53/math-game

---
+++ MathGame/GameMode.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+namespace MathGame
+{
+    abstract class GameMode
+    {
+        int Score { get; set; } = 0;
+        int timeElapsed { get; set; }
+        string? playerName { get; set; }
+        public enum Difficulity { Easy, Medium, Hard };
+
+        public GameMode(Difficulity level = Difficulity.Easy) { }
+
+        public void Start() { }
+
+        public void OnGameOver() { }
+
+        public void SaveScore()
+        {
+            string jsonString = JsonSerializer.Serialize(this);
+        }
+
+    }
+}
---
+++ MathGame/Menu.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathGame
+{
+    internal class Menu
+    {
+        private bool displayMenu = true;
+        public void ShowMenu()
+        {
+            
+            while (displayMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Hello! Do you want to play a game? Select one below!");
+                Console.WriteLine("1 - Addition Game\n2 - Substraction game\n3 - Multiplication game\n4 - Division game\nQ - Quit Game");
+                ConsoleKey selectedOption = Console.ReadKey().Key;
+
+                switch (selectedOption)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("Add Game");
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Substraction Game");
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine("Multiplication Game");
+                        break;
+                    case ConsoleKey.D4:
+                        Console.WriteLine("Division Game");
+                        break;
+                    case ConsoleKey.D5:
+                        Console.WriteLine("Leader Board");
+                        break;
+                    case ConsoleKey.D6:
+                        Console.WriteLine("Quit");
+                        displayMenu = false;
+                        break;
+                    default:
+                        Console.WriteLine("No menu Option Selected");
+                        break;
+                }
+
+            }
+        }
+    }
+}
